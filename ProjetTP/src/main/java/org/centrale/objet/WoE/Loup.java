fea5589f7c2ef25e0fe9d108ad77dd56c4d2baec
Odyseus2 <hanss@130.66.210.74>
2024-09-20 17:35:35 +0200

    Nouveau monstre

---
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/Loup.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.centrale.objet.WoE;
+
+/**
+ *
+ * @author hanss
+ */
+public class Loup extends Monstre {
+    
+    /**
+     * Créé un loup
+     *
+     * @param pV points de vie
+     * @param dA dégats d'attaque
+     * @param pPar points de parade
+     * @param paAtt probabilité de réussite d'une attaque
+     * @param paPar probabilité de parer une attaque
+     * @param p position
+     */
+    public Loup(int pV, int dA, int pPar, int paAtt, int paPar, Point2D p) {
+        super(pV, dA, pPar, paAtt, paPar, p);
+    }
+
+    public Loup(Monstre p) {
+        super(p);
+    }
+
+    public Loup() {
+    }
+    
+    public void combattre() {
+    System.out.println("Grrr");
+    }
+}
