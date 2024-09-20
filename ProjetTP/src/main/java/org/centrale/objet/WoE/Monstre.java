e7eaf9a570a781a139ec9eb433ca3f474709901e
Odyseus2 <hanss@130.66.210.74>
2024-09-20 14:30:16 +0200

    Documentation et formattage

--- ProjetTP/src/main/java/org/centrale/objet/WoE/Monstre.java
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/Monstre.java
@@ -5,19 +5,35 @@
 package org.centrale.objet.WoE;
 
 /**
- *
+ * Creature de type monstre
  * @author hanss
  */
 public class Monstre extends Creature{
 
+    /**
+     * Créé un monstre à partir de paramètres donnés (voir org.centrale.objet.WoE.Creature.Creature())
+     * @param pV
+     * @param dA
+     * @param pPar
+     * @param paAtt
+     * @param paPar
+     * @param p
+     */
     public Monstre(int pV, int dA, int pPar, int paAtt, int paPar, Point2D p) {
         super(pV, dA, pPar, paAtt, paPar, p);
     }
 
+    /**
+     * Recopie le monstre p
+     * @param p
+     */
     public Monstre(Monstre p) {
         super(p);
     }
 
+    /**
+     * Créé aléatoirement un monstre (voir Creature)
+     */
     public Monstre() {
         super();
     }
