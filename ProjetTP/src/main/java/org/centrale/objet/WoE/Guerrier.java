edc97927deadbcaa34ae8d293b19edf9b6386df7
Odyseus2 <hanss@130.66.210.74>
2024-09-20 17:41:23 +0200

    Combat

--- ProjetTP/src/main/java/org/centrale/objet/WoE/Guerrier.java
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/Guerrier.java
@@ -11,25 +11,34 @@ package org.centrale.objet.WoE;
 public class Guerrier extends Personnage {
 
     /**
-     *
-     * @param n
-     * @param pV
-     * @param dA
-     * @param pPar
-     * @param paAtt
-     * @param paPar
-     * @param dMax
-     * @param p
+     * Créé un guerrier
+     * @param n nom
+     * @param pV points de vie
+     * @param dA dégats d'attaque
+     * @param pPar dégats parés
+     * @param paAtt pourcentage de réussite d'attaque
+     * @param paPar pourcentage de réussite de parade
+     * @param dMax distance attaque maximale
+     * @param p Position
      */
     public Guerrier(String n, int pV, int dA, int pPar, int paAtt, int paPar, int dMax, Point2D p) {
         super(n, pV, dA, pPar, paAtt, paPar, dMax, p);
     }
 
+    /**
+     * Copie le guerrier p
+     * @param p
+     */
     public Guerrier(Personnage p) {
         super(p);
     }
 
+    /**
+     * Guerrier aléatoire
+     */
     public Guerrier() {
     }
     
+    public void combattre(Creature c) {
+    }
 }
