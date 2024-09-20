fc99c42f7a73d442182f0e3b6aee3c15fbe026cf
timot <timot@PtitmotheeComputer>
2024-09-20 18:01:18 +0200

    Création des classes Objet, Epee, et PotionSoin

---
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/Objet.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.centrale.objet.WoE;
+
+/**
+ * Super-classe pour les objets de WoE
+ * 
+ * @author timot
+ */
+public class Objet {
+    
+}
