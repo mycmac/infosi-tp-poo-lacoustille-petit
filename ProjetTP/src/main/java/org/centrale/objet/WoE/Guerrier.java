5d47d6f625e68e4232d293d1a6036303a5a4990e
Odyseus2 <hanss@130.66.210.74>
2024-09-20 17:36:16 +0200

    Nouveau personnage

---
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/Guerrier.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.centrale.objet.WoE;
+
+/**
+ *
+ * @author Ulysse
+ */
+public class Guerrier extends Personnage {
+
+    /**
+     *
+     * @param n
+     * @param pV
+     * @param dA
+     * @param pPar
+     * @param paAtt
+     * @param paPar
+     * @param dMax
+     * @param p
+     */
+    public Guerrier(String n, int pV, int dA, int pPar, int paAtt, int paPar, int dMax, Point2D p) {
+        super(n, pV, dA, pPar, paAtt, paPar, dMax, p);
+    }
+
+    public Guerrier(Personnage p) {
+        super(p);
+    }
+
+    public Guerrier() {
+    }
+    
+}
