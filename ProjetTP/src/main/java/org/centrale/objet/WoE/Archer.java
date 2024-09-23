a266aec1c190e479ca34061daa331cdb39d1dca0
Odyseus2 <hanss@130.66.210.74>
2024-09-23 18:27:28 +0200

    Fonction utilitaire

--- ProjetTP/src/main/java/org/centrale/objet/WoE/Archer.java
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/Archer.java
@@ -70,7 +70,11 @@ public class Archer extends Personnage implements Combatif {
         super.affiche();
         System.out.print(this.getNom() + " maîtrise l'archerie et a " + getNbFleches() + " flèches.\n");
     }
-
+    
+    public void utilFleche(){
+    setNbFleches(getNbFleches() - 1);
+    }
+    
     /**
      * Combattre une créature désignée
      *
@@ -84,7 +88,7 @@ public class Archer extends Personnage implements Combatif {
             Epee arme = this.getArme();
             
             if (arme != null) {
-                pAtt += (int) (100-pAtt)*arme.getPageAtt();
+                pAtt += (int) (100-pAtt)*arme.getPageAtt(); // Calcul des stats d'attaque avec arme
             }
             
             if (this.lanceDe(pAtt)) {
