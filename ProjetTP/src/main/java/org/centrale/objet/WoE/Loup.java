d673ee42b50ab6f78e9ffcc5b302c2ad182c0109
Odyseus2 <hanss@130.66.210.74>
2024-09-23 17:38:03 +0200

    Description situations de bataille

--- ProjetTP/src/main/java/org/centrale/objet/WoE/Loup.java
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/Loup.java
@@ -33,17 +33,26 @@ public class Loup extends Monstre implements Combatif {
      * @param c
      */
     public void combattre(Creature c) {
-        if (distance(c)<=1){
-        if (lanceDe(this.getPageAtt())){
-            if (c.lanceDe(c.getPagePar())){
-                int pertes = Math.max(0, this.getDegAtt()-c.getPtPar());
-                c.setPtVie(c.getPtVie()-pertes);}
-            else{c.setPtVie(c.getPtVie()-this.getDegAtt());}
-            if (c.getPtVie()<0){c.setPtVie(0);}
-        }else{
-        System.out.print("Grrr ! Le Loup s'éloigne.");
+        if (distance(c) <= 1) {
+            if (lanceDe(this.getPageAtt())) {
+                if (c.lanceDe(c.getPagePar())) {
+                    int pertes = Math.max(0, this.getDegAtt() - c.getPtPar());
+                    c.setPtVie(c.getPtVie() - pertes);
+                    System.out.println("Le loup attaque "+c+" qui pare son attaque!");
+                } else {
+                    c.setPtVie(c.getPtVie() - this.getDegAtt());
+                    System.out.println("Le loup attaque "+c+" qui ne parvient pas à parer !");
+                }
+                if (c.getPtVie() < 0) {
+                    c.setPtVie(0);
+                    System.out.println(c + " est mort!");
+                }
+            } else {
+                System.out.print("Grrr ! Le Loup s'éloigne.");
+            }
+        } else {
+            System.out.print("Un Loup grogne dans le lointain.");
         }
-        }else{System.out.print("Un Loup grogne dans le lointain.");}
     }
 
     @Override
@@ -51,7 +60,7 @@ public class Loup extends Monstre implements Combatif {
         super.affiche();
         System.out.print("C'est un loup terrifiant !");
     }
-    
+
     @Override
     public String toString() {
         return "Loup";
