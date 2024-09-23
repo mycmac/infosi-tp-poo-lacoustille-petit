6e442f156db9176d0b0dd69d29bab637ce59282d
Odyseus2 <hanss@130.66.210.74>
2024-09-23 13:25:25 +0200

    Formatage

--- ProjetTP/src/main/java/org/centrale/objet/WoE/Guerrier.java
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/Guerrier.java
@@ -10,8 +10,11 @@ package org.centrale.objet.WoE;
  */
 public class Guerrier extends Personnage {
 
+    private int rage;
+
     /**
      * Initialise un guerrier
+     *
      * @param n nom
      * @param pV points de vie
      * @param dA distance d'attaque
@@ -24,28 +27,30 @@ public class Guerrier extends Personnage {
     public Guerrier(String n, int pV, int dA, int pPar, int paAtt, int paPar, int dMax, Point2D p) {
         super(n, pV, dA, pPar, paAtt, paPar, dMax, p);
     }
-    
+
     /**
      * Crée un guerrier identique au guerrier p
+     *
      * @param p Guerrier à copier
      */
     public Guerrier(Personnage p) {
         super(p);
     }
-    
+
     /**
      * Génère un guerrier aléatoire
      */
     public Guerrier() {
         super();
     }
-    
+
     /**
      * Combattre une créature désignée
-     * @param c 
+     *
+     * @param c
      */
     public void combattre(Creature c) {
         System.out.println("Aiyah");
     }
-    
+
 }
