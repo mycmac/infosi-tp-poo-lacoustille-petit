12bba0a1b5e81fd8b9e2aac0702ab659b76806be
timot <timot@PtitmotheeComputer>
2024-09-23 13:24:57 +0200

    Travail sur les classes Objet

--- ProjetTP/src/main/java/org/centrale/objet/WoE/PotionSoin.java
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/PotionSoin.java
@@ -4,10 +4,41 @@
  */
 package org.centrale.objet.WoE;
 
+
 /**
  *
  * @author timot
  */
 public class PotionSoin extends Objet {
     
+    /**
+     * Points de vie de départ : entre 1 et 99
+     */
+    private final int ptVieBaseMin = 1;
+    /** @see #ptVieBaseMin */
+    private final int ptVieBaseMax = 99;
+    
+    private int ptVie;
+    
+    public PotionSoin(int pv){
+        ptVie = pv;
+    }
+    
+    public PotionSoin(PotionSoin p){
+        ptVie = p.getPtVie();
+    }
+    
+    public PotionSoin(){
+        Random r = new Random();
+        
+    }
+    
+    public int getPtVie(){
+        return ptVie;
+    }
+    
+    @Override
+    public void recuperer(Personnage p){
+        // TODO
+    }
 }
