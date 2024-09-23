697c8b0e8e8fa989ac29e50956861f2b49d54f5e
Odyseus2 <hanss@130.66.210.74>
2024-09-23 13:56:51 +0200

    Ajout de l'interface Combatif

---
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/Combatif.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package org.centrale.objet.WoE;
+
+/**
+ *
+ * @author hanss
+ */
+public interface Combatif {
+    public void combattre(Creature c);
+}
