e0b059e577daa6f8e73f6580bc00f832e59b9d8e
timot <timot@PtitmotheeComputer>
2024-09-23 18:41:12 +0200

    Combattre

--- ProjetTP/src/main/java/org/centrale/objet/WoE/Archer.java
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/Archer.java
@@ -87,7 +87,8 @@ public class Archer extends Personnage implements Combatif {
     public void combattre(Creature c) {
         String msg = new String();
         int dgts = 0;
-        if (this.distance(c) <= 1) {
+        double d = this.distance(c);
+        if (d <= 1) {
             int pAtt = this.getDegAtt();
             Epee arme = this.getArme();
             
@@ -134,7 +135,7 @@ public class Archer extends Personnage implements Combatif {
             } else {
                 msg = "Le coup a raté.";
             }
-        } else {
+        } else if (d <= this.getDistAttMax()) {
             if (this.getNbFleches() == 0) {
                 msg = "Vous n'avez plus de flèches !";
             } else {
@@ -148,6 +149,8 @@ public class Archer extends Personnage implements Combatif {
                     msg = "La flèche a raté.";
                 }
             }
+        } else {
+            msg = "La cible est trop loin.";
         }
     }
 }
