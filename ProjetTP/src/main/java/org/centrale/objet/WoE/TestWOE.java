17ce8091d8f8d9e7dd2d31d77891ae18afb7b6f7
Odyseus2 <hanss@130.66.210.74>
2024-09-30 16:05:30 +0200

    Tests de temps

--- ProjetTP/src/main/java/org/centrale/objet/WoE/TestWOE.java
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/TestWOE.java
@@ -12,25 +12,8 @@ public class TestWOE {
         World WoE;
         WoE = new World();
         WoE.creeMondeAlea();
-        System.out.println("Positions objets : ");
-        for (Objet o : WoE.objets) {
-            System.out.println(o.getPos());
-        }
-        System.out.println("Positions objets récupérés : ");
-        System.out.println(WoE.objets.get(1).getPos());
-        System.out.println(WoE.objets.get(2).getPos());
-        // Parcours de la liste pour supprimer les objets récupérés.
-        Iterator<Objet> listIt = WoE.objets.iterator();
-
-        while (listIt.hasNext()) {
-            Objet p = listIt.next();
-            if (p.getPos() == null) {
-                listIt.remove();
-            }
-        }
-        System.out.println("Positions objets : ");
-        for (Objet o : WoE.objets) {
-            System.out.println(o.getPos());
-        }
+        long debut = System.nanoTime();
+        
+        long fin = System.nanoTime();
     }
 }
