5bd4f564a548540e47b3ef834a9d1406bac65835
timot <timot@PtitmotheeComputer>
2024-09-30 22:56:16 +0200

    Surcharge pour définition par position (x, y) ou p Complétion de Javadoc

--- ProjetTP/src/main/java/org/centrale/objet/WoE/Objet.java
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/Objet.java
@@ -7,17 +7,45 @@ package org.centrale.objet.WoE;
  */
 public abstract class Objet extends Entite {
     
+    /**
+     * Initialise un Objet comme copie d'un autre
+     * 
+     * @param o 
+     */
     public Objet(Objet o) {
         super(o.getPos(), false);
     }
     
+    /**
+     * Génère un Objet au point (x, y)
+     * 
+     * @param x abscisse
+     * @param y ordonnée
+     */
+    public Objet(int x, int y) {
+        super(new Point2D(x, y), false);
+    }
+    
+    /**
+     * Génère un Objet au point pos
+     * 
+     * @param pos point
+     */
     public Objet(Point2D pos) {
         super(pos, false);
     }
     
+    /**
+     * Génère un Objet aléatoire
+     */
     public Objet() {
         super(new Point2D(), false);
     }
 
+    /**
+     * Comportement de l'objet lorsqu'il est récupéré par un personnage
+     * 
+     * @param p Personnage
+     */
     public abstract void recuperer(Personnage p);
 }
