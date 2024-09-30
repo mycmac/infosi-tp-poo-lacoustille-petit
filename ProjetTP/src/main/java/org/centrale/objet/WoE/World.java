30f97f7ac9e148ee8db7fa0f2a3a31d0616f6817
Odyseus2 <hanss@130.66.210.74>
2024-09-30 17:00:21 +0200

    Utilisation grille de vérification personnages

--- ProjetTP/src/main/java/org/centrale/objet/WoE/World.java
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/World.java
@@ -2,7 +2,6 @@ package org.centrale.objet.WoE;
 
 import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.ArrayList;
 import java.util.Random;
 
 /**
@@ -16,64 +15,57 @@ public class World {
     Random seed;
     boolean[][] grille;
     int taille;
-    
+
     /**
      * Monde avec personnages par défaut
      */
     public World() {
-        this.creatures = new LinkedList<Creature>();
-        this.objets = new LinkedList<Objet>();
+        this.creatures = new LinkedList<>();
+        this.objets = new LinkedList<>();
         this.seed = new Random();
     }
 
     /**
      * Création d'un monde aléatoire
+     *
+     * @param taille Taille du monde
      */
     public void creeMondeAlea(int taille) {
+        this.taille = taille;
         int x;
         int y;
         boolean pris;
         this.grille = new boolean[taille][taille];
-        System.out.print(grille);
         Iterator<Creature> CreaIt1 = this.creatures.iterator();
         Iterator<Creature> CreaIt2;
         Creature c1;
         Creature c2;
-        while(CreaIt1.hasNext()) {
+        while (CreaIt1.hasNext()) {
             c1 = CreaIt1.next();
             x = 0;
             y = 0;
             pris = true;
-            
+
             while (pris) {
-                pris = false;
-                x = this.seed.nextInt(taille);
-                y = this.seed.nextInt(taille);
-                CreaIt2 = this.creatures.iterator();
-                c2 = CreaIt2.next();
-                while (c2 != c1) {
-                    if (c2.getX() == x && c2.getY() == y) {
-                        pris = true;
-                    }
-                    c2 = CreaIt2.next();
-                }
+                x = this.seed.nextInt(this.taille);
+                y = this.seed.nextInt(this.taille);
+                pris = this.grille[x][y];
             }
             c1.setPos(x, y);
             this.grille[x][y] = true;
             c1.affiche();
         }
-        
 
         Iterator<Objet> ObjIt1 = this.objets.iterator();
         Iterator<Objet> ObjIt2;
         Objet o1;
         Objet o2;
-        while(ObjIt1.hasNext()) {
+        while (ObjIt1.hasNext()) {
             o1 = ObjIt1.next();
             x = 0;
             y = 0;
             pris = true;
-            
+
             while (pris) {
                 pris = false;
                 x = this.seed.nextInt(taille);
@@ -86,7 +78,7 @@ public class World {
                     }
                     o2 = ObjIt2.next();
                 }
-                
+
                 CreaIt2 = this.creatures.iterator();
                 while (CreaIt2.hasNext() && !pris) {
                     c2 = CreaIt2.next();
@@ -109,7 +101,7 @@ public class World {
         cleanEntites(creatures);
         cleanEntites(objets);
         for (Creature creature : creatures) {
-            System.out.println("C'est au tour de "+creature+" de jouer.");
+            System.out.println("C'est au tour de " + creature + " de jouer.");
             creature.deplace();
             creature.affiche();
         }
@@ -125,6 +117,7 @@ public class World {
 
     /**
      * Retire les objets utilisés (sans position)
+     *
      * @param Liste
      */
     public void cleanEntites(LinkedList Liste) {
