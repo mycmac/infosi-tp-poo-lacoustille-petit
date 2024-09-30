86dbff7ba3c67318a4a30b63d8cb7d107827c84a
Odyseus2 <hanss@130.66.210.74>
2024-09-30 23:10:29 +0200

    Formattage

--- ProjetTP/src/main/java/org/centrale/objet/WoE/Archer.java
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/Archer.java
@@ -36,10 +36,10 @@ public class Archer extends Personnage implements Combatif {
         super(a);
         this.nbFleches = a.getNbFleches();
     }
-    
+
     /**
      * Crée un Archer aléatoire au point (x, y) donné
-     * 
+     *
      * @param x abscisse
      * @param y ordonnée
      */
@@ -47,10 +47,10 @@ public class Archer extends Personnage implements Combatif {
         super(x, y);
         this.nbFleches = getRandom(17);
     }
-    
+
     /**
      * Crée un Archer aléatoire au point p donné
-     * 
+     *
      * @param p point
      */
     public Archer(Point2D p) {
@@ -91,14 +91,14 @@ public class Archer extends Personnage implements Combatif {
         super.affiche();
         System.out.print(this.getNom() + " maîtrise l'archerie et a " + getNbFleches() + " flèches.\n");
     }
-    
+
     /**
      * Utilise une flèche
      */
-    public void utilFleche(){
-    setNbFleches(getNbFleches() - 1);
+    public void utilFleche() {
+        setNbFleches(getNbFleches() - 1);
     }
-    
+
     /**
      * Combattre une créature désignée
      *
@@ -112,46 +112,46 @@ public class Archer extends Personnage implements Combatif {
         if (d <= 1) {
             int pAtt = this.getDegAtt();
             Epee arme = this.getArme();
-            
+
             if (arme != null) {
-                pAtt += (int) (100-pAtt)*arme.getPageAtt(); // Calcul des stats d'attaque avec arme
+                pAtt += (int) (100 - pAtt) * arme.getPageAtt(); // Calcul des stats d'attaque avec arme
             }
-            
+
             if (this.lanceDe(pAtt)) {
                 int dAtt = this.getDegAtt();
-                
+
                 if (arme != null) {
-                    dAtt *= (5+arme.getDegAtt())/10.; // Calcul des dégats avec arme
+                    dAtt *= (5 + arme.getDegAtt()) / 10.; // Calcul des dégats avec arme
                 }
-                
+
                 int pPar = c.getPagePar();
                 boolean cIsPerso = c instanceof Personnage;
                 if (cIsPerso) {
                     Epee cArme = ((Personnage) c).getArme();
                     if (cArme != null) {
-                        pPar += (int) (100-pAtt)*this.getArme().getPagePar();
+                        pPar += (int) (100 - pAtt) * this.getArme().getPagePar();
                     }
                 }
-                
+
                 if (c.lanceDe(pPar)) {
                     int dPar = c.getPtPar();
                     if (cIsPerso) {
                         Epee cArme = ((Personnage) c).getArme();
                         if (cArme != null) {
-                            dPar *= (5+cArme.getPtPar())/10.; 
+                            dPar *= (5 + cArme.getPtPar()) / 10.;
                         }
                     }
-                    dgts = Math.max(0, dAtt-dPar);
+                    dgts = Math.max(0, dAtt - dPar);
                     if (dgts == 0) {
                         msg = "Le coup atteint sa cible mais est complètement bloqué.";
                     } else {
                         msg = "Le coup est bloqué mais inflige tout de même " + dgts + " dégats !";
-                        c.setPtVie(Math.max(0, c.getPtVie()-dgts));
+                        c.setPtVie(Math.max(0, c.getPtVie() - dgts));
                     }
                 } else {
                     dgts = dAtt;
                     msg = "Le coup touche et inflige " + dgts + " dégats !";
-                    c.setPtVie(Math.max(0, c.getPtVie()-dgts));
+                    c.setPtVie(Math.max(0, c.getPtVie() - dgts));
                 }
             } else {
                 msg = "Le coup a raté.";
@@ -164,7 +164,7 @@ public class Archer extends Personnage implements Combatif {
                 this.utilFleche();
                 if (this.lanceDe(pAtt)) {
                     dgts = this.getDegAtt();
-                    c.setPtVie(Math.max(0, c.getPtVie()-dgts));
+                    c.setPtVie(Math.max(0, c.getPtVie() - dgts));
                     msg = "La flèche touche et inflige " + dgts + " dégats !";
                 } else {
                     msg = "La flèche a raté.";
