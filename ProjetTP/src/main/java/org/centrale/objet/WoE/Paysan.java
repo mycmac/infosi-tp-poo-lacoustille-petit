865dfd332c4e4f4de6129549654fe13c43b5c943
timot <timot@PtitmotheeComputer>
2024-09-30 22:40:11 +0200

    Surcharge pour définition par position (x, y) ou p Complétion de Javadoc

--- ProjetTP/src/main/java/org/centrale/objet/WoE/Paysan.java
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/Paysan.java
@@ -5,18 +5,61 @@ package org.centrale.objet.WoE;
  * @author Ulysse
  */
 public class Paysan extends Personnage {
-
-    public Paysan(String n, int pV, int dA, int pPar, int paAtt, int paPar, int dMax, Point2D p) {
-        super(n, pV, dA, pPar, paAtt, paPar, dMax, p);
+    
+    /**
+     * Initialise un Paysan à partir de tous ses attributs
+     * 
+     * @param n nom
+     * @param pV points de vie
+     * @param dA dégâts d'attaque
+     * @param pPar points de parade
+     * @param paAtt pourcentage d'attaque
+     * @param paPar pourcentage de parade
+     * @param dMax distance d'attaque maximale
+     * @param pt point
+     */
+    public Paysan(String n, int pV, int dA, int pPar, int paAtt, int paPar, int dMax, Point2D pt) {
+        super(n, pV, dA, pPar, paAtt, paPar, dMax, pt);
     }
-
-    public Paysan(Personnage p) {
+    
+    /**
+     * Initialise un Paysan comme copie d'un autre paysan
+     * 
+     * @param p Paysan initial
+     */
+    public Paysan(Paysan p) {
         super(p);
     }
-
-    public Paysan() {
+    
+    /**
+     * Génère un Paysan au point (x, y)
+     * 
+     * @param x abscisse
+     * @param y ordonnée
+     */
+    public Paysan(int x, int y) {
+        super(x, y);
     }
-
+    
+    /**
+     * Génère un Paysan au point pt
+     * 
+     * @param pt point
+     */
+    public Paysan(Point2D pt) {
+        super(pt);
+    }
+    
+    /**
+     * Génère un Paysan aléatoire
+     */
+    public Paysan() {
+        super();
+    }
+    
+    /**
+     * Affiche le Paysan en console
+     */
     @Override
     public void affiche() {
         super.affiche();
