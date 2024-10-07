1af94c2b1d2e3fe2a8843681333b19e94aeac5d2
Odyseus2 <hanss@130.66.210.74>
2024-10-07 18:07:15 +0200

    Ajout nuage toxique

---
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/NuageToxique.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.centrale.objet.WoE;
+
+/**
+ *
+ * @author hanss
+ */
+public class NuageToxique extends Objet implements Deplacable, Combatif {
+    private int toxicite;
+
+    public NuageToxique(int toxicite, int x, int y) {
+        super(x, y);
+        this.toxicite = toxicite;
+    }
+
+    public NuageToxique(int toxicite, Point2D pos) {
+        super(pos);
+        this.toxicite = toxicite;
+    }
+
+    public int getToxicite() {
+        return toxicite;
+    }
+
+    public void setToxicite(int toxicite) {
+        this.toxicite = toxicite;
+    }
+    
+    
+    @Override
+    public void recuperer(Personnage p) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void deplace(Creature[][] grille) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void deplace(Creature[][] grille, Point2D p) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void combattre(Creature c) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
