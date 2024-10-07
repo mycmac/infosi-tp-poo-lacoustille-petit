6a43514e70e0e186e2daa2566cc7054069f06688
Odyseus2 <hanss@130.66.210.74>
2024-10-07 17:32:22 +0200

    Création interface déplace

---
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/Deplace.java
@@ -0,0 +1,10 @@
+package org.centrale.objet.WoE;
+
+/**
+ *
+ * @author Ulysse
+ */
+public interface Deplace {
+    public void deplace(Creature[][] grille);
+    public void deplace(Creature[][] grille, Point2D p);
+}
