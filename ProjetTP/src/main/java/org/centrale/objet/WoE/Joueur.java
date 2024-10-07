b184835aca380c95f72e4ec6498d3aa4d9082e79
Odyseus2 <hanss@130.66.210.74>
2024-10-07 17:27:11 +0200

    Création joueur

---
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/Joueur.java
@@ -0,0 +1,12 @@
+package org.centrale.objet.WoE;
+
+/**
+ *
+ * @author Ulysse
+ */
+public class Joueur {
+    private Personnage perso;
+    public void Joueur(Personnage p) {
+        perso = p;
+    }
+}
