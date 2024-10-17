5cb993c3e2e4c9b38f74fc6c2483dcf893f74088
Odyseus2 <hanss@130.66.210.74>
2024-10-17 18:42:28 +0200

    Déplacement créature jouable

--- ProjetTP/src/main/java/org/centrale/objet/WoE/Jouable.java
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/Jouable.java
@@ -9,5 +9,13 @@ package org.centrale.objet.WoE;
  * @author hanss
  */
 public interface Jouable {
+
+    /**
+     * Déplace de manière donnée une créature jouable
+     * @param grille grille de déplacement
+     * @param dx déplacement X
+     * @param dy déplacement Y
+     */
+    public void deplace(Creature[][] grille, int dx, int dy);
     
 }
