1dcad10340fda24119adee864862a9daf327b9d6
timot <timot@PtitmotheeComputer>
2024-10-18 12:21:16 +0200

    Javadoc

--- ProjetTP/src/main/java/org/centrale/objet/WoE/Types_Nourriture.java
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/Types_Nourriture.java
@@ -7,6 +7,9 @@ import java.util.Random;
 
 import org.centrale.objet.WoE.Creature.Carac;
 
+/**
+ * Enumeration des types de nourriture prédéterminés
+ */
 public enum Types_Nourriture {
     EPINARD(
         "épinard", 
@@ -29,45 +32,94 @@ public enum Types_Nourriture {
     )
     ;
 
-
+    /**
+     * Liste non modifiable des types d'arme
+     */
     private static final List<Types_Nourriture> VALUES = Collections.unmodifiableList(Arrays.asList(values()));
+    /**
+     * Taille de la liste des types d'arme
+     */
     private static final int SIZE = VALUES.size();
+    /**
+     * Générateur de nombres aléatoires
+     */
     private static final Random RANDOM = new Random();
-
+    
+    /**
+     * Nom de la nourriture
+     */
     private String nom;
+    /**
+     * Bonus associé à la nourriture
+     */
     private Modificateur bonus;
+    /**
+     * Malus associés à la nourriture
+     */
     private Modificateur[] malus;
-
+    
+    /**
+     * Constructeur de Types_Nourriture avec tous les attributs
+     * @param nom
+     * @param bonus
+     * @param malus
+     */
     private Types_Nourriture(String nom, Modificateur bonus, Modificateur[] malus) {
         this.nom = nom;
         this.bonus = bonus;
         this.malus = malus;
     }
 
+    /**
+     * Constructeur de Types_Nourriture
+     * @param nom
+     * @param bonus
+     */
     private Types_Nourriture(String nom, Modificateur bonus) {
         this.nom = nom;
         this.bonus = bonus;
         this.malus = null;
     }
 
+    /**
+     * Constructeur de Types_Nourriture
+     * @param nom
+     * @param malus
+     */
     private Types_Nourriture(String nom, Modificateur[] malus) {
         this.nom = nom;
         this.bonus = null;
         this.malus = malus;
     }
 
+    /**
+     * Renvoie le nom d'un type de nourriture
+     * @return nom
+     */
     public String getNom() {
         return this.nom;
     }
 
+    /**
+     * Renvoie le bonus associé au type de nourriture
+     * @return bonus
+     */
     public Modificateur getBonus() {
         return this.bonus;
     }
 
+    /**
+     * Renvoie les malus associés au type de nourriture
+     * @return malus
+     */
     public Modificateur[] getMalus() {
         return this.malus;
     }
 
+    /**
+     * Donne un type de nourriture aléatoire
+     * @return Type de nourriture aléatoire
+     */
     public static Types_Nourriture getRandom() {
         return VALUES.get(RANDOM.nextInt(SIZE));
     }
