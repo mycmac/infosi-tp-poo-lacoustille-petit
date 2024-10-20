d77c32734bf1b51f8c9bd022d5a6c1e9439c712e
Odyseus2 <hanss@130.66.210.74>
2024-10-20 18:49:30 +0200

    woe final II

--- ProjetTP/src/main/java/org/centrale/objet/WoE/woe.java
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/woe.java
@@ -12,6 +12,7 @@ public class woe {
      * @param args
      */
     public static void main(String[] args) {
+        // Initialisation du monde
         int taille;
         try {
             taille = Integer.parseInt(args[0]);
@@ -24,10 +25,12 @@ public class woe {
         Fenetre.Initialize(WoE.getTaille());
         Fenetre.addMessage("Bienvenue dans World Of ECN !");
         boolean vivant = true;
+        // Moteur de jeu
         while (vivant) {
             vivant = WoE.tourDeJeu();
             WoE.afficheWorld();
         }
+        //Decès
         Fenetre.addMessage("\n\n\nVous êtes mort!\n");
 
     }
