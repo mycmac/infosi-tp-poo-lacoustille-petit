29c2f6741d48c28d890cecad402d0a3b88c87d07
Odyseus2 <hanss@130.66.210.74>
2024-10-20 19:08:49 +0200

    Recuperable final

--- ProjetTP/src/main/java/org/centrale/objet/WoE/Recuperable.java
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/Recuperable.java
@@ -1,10 +1,11 @@
 package org.centrale.objet.WoE;
 
 public interface Recuperable {
+
     /**
      * Comportement de l'objet lorsqu'il est récupéré par un personnage
-     * 
-     * @param p Personnage
+     *
+     * @param j Joueur qui récupère l'objet
      */
     public abstract void recuperer(Joueur j);
 }
